640d56212180d5f5ea0f2b24995e96eaee1dc2ef
andrewdhazlett <andrew@hazlett.me>
2015-01-09 10:11:23 -0500

    echo bootstrapping script steps

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #! /bin/zsh
 
-# init and update git submodules
+echo 'init and update git submodules'
 git submodule update --init;
 
-# get the directory where the repo was cloned 
+echo 'get the directory where the repo was cloned'
 DIR=`git rev-parse --show-toplevel`;
 OSX_DIR=$DIR'/osx'
 
-# set up links to submodule directories
+echo 'set up links to submodule directories'
 ln -s $DIR/vim/.vim ~
 ln -s $DIR/zsh/prezto ~/.zprezto
 setopt EXTENDED_GLOB
@@ -15,12 +15,12 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-# init prezto
+echo 'init prezto'
 source $DIR/zsh/prezto/init.zsh
-# init powerline fonts
+echo 'init powerline fonts'
 source $DIR/powerline-fonts/install.sh
 
-# os-specific config
+echo 'os-specific config'
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	ln -s $DIR/.linux_aliases ~
@@ -33,13 +33,13 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 # else
 fi
 
-# set up links for config files
+echo 'set up links for config files'
 ln -s $DIR/.aliases ~
 ln -s $DIR/.bashrc ~
 ln -s $DIR/git/.gitconfig ~
 ln -s $DIR/.tmux.conf ~
 ln -s $DIR/vim/.vimrc ~
-ln -s $DIR/zsh/.zshrc ~
+#ln -s $DIR/zsh/.zshrc ~
 
-# npm install where necessary
+echo 'npm install where necessary'
 cd $DIR/vim/.vim/bundle/tern_for_vim && npm i
