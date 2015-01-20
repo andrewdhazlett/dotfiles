726c98704c839fea6f51d74af5fe41d0b2802dbf
andrewdhazlett <andrew@hazlett.me>
2015-01-20 17:43:09 -0500

    move prezto

--- config_zsh.sh
+++ config_zsh.sh
@@ -1,10 +1,11 @@
-#! /bin/sh
+#! /bin/zsh
 echo 'set up links to submodule directories'
-ln -s $DIR/zsh/prezto ~/.zprezto
+ln -s $DIR/zsh/.zprezto ~
+
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 echo 'init prezto'
-source $DIR/zsh/prezto/init.zsh
+source $DIR/zsh/.zprezto/init.zsh
