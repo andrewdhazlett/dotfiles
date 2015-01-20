dbc920445b18c4cc5f268003e1a862f373a378df
andrewdhazlett <andrew@hazlett.me>
2015-01-20 18:31:40 -0500

    move zsh and prezto dotfiles into ./zsh

--- config_zsh.sh
+++ config_zsh.sh
@@ -3,12 +3,9 @@
 DIR=`git rev-parse --show-toplevel`
 
 echo 'set up links to submodule directories'
+ln -s $DIR/zsh/.zlogin ~
+ln -s $DIR/zsh/.zlogout ~
 ln -s $DIR/zsh/.zprezto ~
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-	ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-echo 'init prezto'
-source $DIR/zsh/.zprezto/init.zsh
+ln -s $DIR/zsh/.zpreztorc ~
+ln -s $DIR/zsh/.zprofile ~
+ln -s $DIR/zsh/.zshrc ~
