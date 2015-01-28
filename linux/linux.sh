f776a9904974758d000a30d2d6314f8788109ed8
andrewdhazlett <andrew@hazlett.me>
2015-01-28 11:14:45 -0500

    add aptitude packages required for ycm build

--- linux/linux.sh
+++ linux/linux.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
-apt-get install cmake
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gcc make build-essential cmake nodejs mongodb python-dev redis-server
+#ln -s /usr/bin/nodejs /usr/bin/node
 #ln -s $LINUX_DIR/.linux_aliases ~
