833b837c82bd784c32f3b837d475973750d24f5b
andrewdhazlett <andrew@hazlett.me>
2015-03-17 09:51:51 -0400

    move tmux conf to separate dir

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
+echo ''
 echo 'init and update git submodules'
 git submodule update --init --recursive
 
+echo ''
 echo 'get the directory where the repo was cloned'
 DIR=`git rev-parse --show-toplevel`
 OSX_DIR=$DIR'/osx'
 LINUX_DIR=$DIR'/linux'
 
+echo ''
 echo 'init powerline fonts'
 source $DIR/powerline-fonts/install.sh
 
@@ -23,6 +26,10 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 echo ''
+echo 'set up tmux'
+cd $DIR/tmuxconf && $DIR/tmuxconf/install.sh
+
+echo ''
 echo 'set up vim'
 cd $DIR/vimrc && $DIR/vimrc/install.sh
 
@@ -30,13 +37,15 @@ echo ''
 echo 'set up zsh'
 cd $DIR/zsh && $DIR/zsh/install.sh
 
+echo ''
 echo 'set up links for config files'
 ln -s $DIR/git/.gitconfig ~
 ln -s $DIR/.npmrc ~
-ln -s $DIR/.tmux.conf ~
 
+echo ''
 echo 'update npm'
 npm install -g npm@latest
 
+echo ''
 echo 'npm install where necessary'
 cd $DIR/vim/.vim/bundle/tern_for_vim && npm i
---
+++ tmuxconf/install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+TMUX_DIR=`pwd`
+ln -s $TMUX_DIR/.tmux.conf ~
+
+# return to DIR
+cd $DIR
