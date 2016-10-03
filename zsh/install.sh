37d859f49556fb7ff4919ce7cdbbb84a34f6967f
andrewdhazlett <andrew@hazlett.me>
2016-10-03 16:57:41 -0400

    clean up zsh/prezto config

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,11 +3,18 @@
 ZSH_DIR=`pwd`
 
 echo ''
-echo 'set up links to submodule directories'
-ln -s $ZSH_DIR/.aliases ~
-ln -s $ZSH_DIR/.zlogin ~
-ln -s $ZSH_DIR/.zlogout ~
+echo 'clone prezto'
+git clone --recursive https://github.com/sorin-ionescu/prezto.git .zprezto
+
+echo ''
+echo 'set up zsh links'
+# link prezto
 ln -s $ZSH_DIR/.zprezto ~
+
+# link internal prezto conf
+ln -s $ZSH_DIR/.zprezto/runcoms/zlogin ~/.zlogin
+ln -s $ZSH_DIR/.zprezto/runcoms/zprofile ~/.zprofile
+
+# link custom config
 ln -s $ZSH_DIR/.zpreztorc ~
-ln -s $ZSH_DIR/.zprofile ~
 ln -s $ZSH_DIR/.zshrc ~
