9b77092d00d2ad97d92d0628bd7a233843f24872
Andrew Hazlett <andrew@hazlett.me>
2020-12-19 22:37:03 +0000

    add -y flag to apt commands

--- linux/linux.sh
+++ linux/linux.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install gcc make build-essential cmake nodejs mongodb python-dev redis-server
+sudo apt-get upgrade -y
+sudo apt-get install -y gcc make build-essential cmake nodejs mongodb python-dev redis-server
 #ln -s /usr/bin/nodejs /usr/bin/node
 #ln -s $LINUX_DIR/.linux_aliases ~
