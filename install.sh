43bf3b22d9e144e7f91fcdb4acd38e140605d871
andrewdhazlett <andrew@hazlett.me>
2021-11-21 14:36:39 -0500

    rm macos config

--- install.sh
+++ install.sh
@@ -12,10 +12,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	ln -s $LINUX_DIR/.linux_aliases ~
 	ln -s $LINUX_DIR/.tmux.conf.linux ~/.tmux.conf.local
 	source $LINUX_DIR/linux.sh
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	MACOS_DIR=$DIR'/macos'
-	ln -s $MACOS_DIR/.tmux.conf.macos ~/.tmux.conf.local
-	source $MACOS_DIR/macos.sh
 fi
 
 links=(
